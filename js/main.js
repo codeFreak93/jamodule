5856b7a4da4967d7180ad0eb5c858f6d63844ce0
Juan de Dios <juand10193@hotmail.com>
2020-12-07 22:25:03 -0600

    ADD Indtroduction JS

---
+++ js/main.js
@@ -0,0 +1,46 @@
+console.log('Hola Koders')
+//alert('Hola Koders')
+
+//variables
+var nombreVariable;
+let nombreVariable2;
+
+
+let nombreVariable3 = 'Algo';
+let numero = 1000;
+let boleano = true;
+let myArray = [1,'1',true];
+let myObjectKoder = {
+    nombre: 'jorge'
+}
+
+//Const
+const NAME_KODER = 'Oscar';
+let name_koder =prompt('¿Cuál es tu nombre?')
+
+//Case Styles
+//camelCase
+//PascalCase
+//snake_case
+//kebab-case
+
+let myObject ={
+    html:{
+        head:{},
+        body:{}
+    }
+}
+
+//rem root element
+//em element
+let age_koder = prompt('¿Cuál es tu edad?')
+let resultSuma = parseInt(age_koder);
+
+console.log(name_koder)
+console.log('suma: ' + name_koder + 5);
+console.log('resta: ' + name_koder - 5);
+console.log('multiplicacion: ' + name_koder * 5);
+console.log('Division: ' + name_koder / 5);
+
+
+
