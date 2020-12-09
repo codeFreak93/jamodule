c5399bf016925e60e3cef15aed4054f762b0b77e
Juan de Dios <juand10193@hotmail.com>
2020-12-08 19:51:15 -0600

    ADD backtics

--- js/weight_in_moon.js
+++ js/weight_in_moon.js
@@ -1,3 +1,5 @@
 let  peso = prompt('¿Cual es tu peso?');
 let pesoEnLaLuna = (peso/9.81)*1.622;
-alert('Tu peso en la Luna es :' +  pesoEnLaLuna + 'Kg');
+let result ='Tu peso en la Luna es :' +  pesoEnLaLuna + 'Kg';
+let result2 = `Tu peso en la Luna es : ${pesoEnLaLuna} Kg`
+alert(result2);
