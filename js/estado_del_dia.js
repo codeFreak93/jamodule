4d95ecb730408b9152b3e26449daf2e37bfda5a4
Juan de Dios <juand10193@hotmail.com>
2020-12-08 21:10:29 -0600

    MOD estado_del_dia console

--- js/estado_del_dia.js
+++ js/estado_del_dia.js
@@ -1,15 +1,15 @@
 const ESTADO =prompt('¿Como esta el dia?');
 if(ESTADO =='lluvioso' && ESTADO!=null){
 
-    alert(`El dia esta ${ESTADO} por aqui`);
+    console.log(`El dia esta ${ESTADO} por aqui`);
 }else if(ESTADO == 'soleado' && ESTADO!=null){
-    alert(`ÈL dia esta ${ESTADO} por aqui`);
+    console.log(`ÈL dia esta ${ESTADO} por aqui`);
 }else if(ESTADO == 'nevado' && ESTADO!=null){
-    alert(`El dia esta ${ESTADO} por aqui`);
+    console.log(`El dia esta ${ESTADO} por aqui`);
 
 }else if(ESTADO =='nublado' && ESTADO!=null){
-    alert(`El dia esta ${ESTADO} por aqui`);
+    console.log(`El dia esta ${ESTADO} por aqui`);
 }else{
     let descripcion =prompt('Ingrese la descripcion del dia');
-    alert(`El dia esta ${descripcion} por aqui`);
+    console.log(`El dia esta ${descripcion} por aqui`);
 }
