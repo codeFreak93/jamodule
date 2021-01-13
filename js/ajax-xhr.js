118f9c996699921852d3533ebf1811f1ef7fdc39
Juan de Dios Hernandez <juand10193@hotmail.com>
2021-01-12 18:06:36 -0600

    Update class

--- js/ajax-xhr.js
+++ js/ajax-xhr.js
@@ -43,8 +43,137 @@ const printPosts = (selector, arrData) => {
 }
 
 
-// loadUsers()
+// nuevo usuario
+const urlDb ='https://koders1gpython-default-rtdb.firebaseio.com/jorge/'
+let newUser ={
+    id:1,
+    lastname:'Camarillo',
+    name:'Jorge',
+    uslPhoto:''
+}
 
+// Create user
+const createUser  = () => {
+    // https://koders1gpython-default-rtdb.firebaseio.com/jorge
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(this.response))
+        } else if (this.readyState === 4 && this.status === 404) {
+            console.error(JSON.parse(this.response))
+            return false
+        }
+    }
+    xhttp.open('POST', 'https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/.json')
+    xhttp.send(
+        JSON.stringify({
+            id: 1,
+            name: "Jorge",
+            lastname: "Camarillo",
+            urlPhoto: "https://loremflickr.com/320/240/person"
+        })
+    )
+}
+// Read user
+const    readUser  = () => {
+    // https://koders1gpython-default-rtdb.firebaseio.com/jorge
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(this.response))
+        } else if (this.readyState === 4 && this.status === 404) {
+            console.error(JSON.parse(this.response))
+            return false
+        }
+    }
+    xhttp.open('GET', 'https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/.json')
+    // xhttp.open('GET', 'https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/-MQnTbsClYPUsYDcBS-R.json')
+    xhttp.send()
+}
+// Update and replace
+const updateAndReplaceUser  = (idUser) => {
+    // https://koders1gpython-default-rtdb.firebaseio.com/jorge
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log(JSON.parse(this.response))
+      } else if (this.readyState === 4 && this.status === 404) {
+        console.error(JSON.parse(this.response))
+        return false
+      }
+    }
+    xhttp.open('PUT', `https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/${idUser}.json`)
+    xhttp.send(
+      JSON.stringify({
+        id: 1,
+        name: "Jorge",
+        lastname: "Camarillo",
+        urlPhoto: "https://loremflickr.com/320/240/person"
+      })
+    )
+}
+// Update and modify 
+const updateAndModifyUser  = (idUser) => {
+    // https://koders1gpython-default-rtdb.firebaseio.com/jorge
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log(JSON.parse(this.response))
+      } else if (this.readyState === 4 && this.status === 404) {
+        console.error(JSON.parse(this.response))
+        return false
+      }
+    }
+    xhttp.open('PATCH', `https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/${idUser}.json`)
+    xhttp.send(
+      JSON.stringify({
+        name: "Jorge Luis"
+      })
+    )
+}
+// Delete 
+const deleteUser  = (idUser) => {
+    // https://koders1gpython-default-rtdb.firebaseio.com/jorge
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+        console.log(JSON.parse(this.response))
+        } else if (this.readyState === 4 && this.status === 404) {
+        console.error(JSON.parse(this.response))
+        return false
+        }
+    }
+    xhttp.open('DELETE', `https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/${idUser}.json`)
+    xhttp.send()
+}
+
+const requestAjax  = (url,callback, method='GET', json={} )=>{
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(this.response))
+            callback(JSON.parse(this.response))
+            return true
+        } else if (this.readyState === 4 && this.status === 404) {
+            console.error(JSON.parse(this.response))
+            return false
+        }
+    }
+
+    xhttp.open(method, url)
+
+    if(method !=='GET' && method !=='DELETE'){
+       
+        xhttp.send(JSON.stringify(json))
+    }else{
+        xhttp.send()
+    }   
+    
+}
+
+const showREsponse =(response)=>{
+    console.log(response)
+}
 document.getElementById('getusers').addEventListener('click', function(){
     getData('https://jsonplaceholder.typicode.com/users', printUsers, '.list__users')
 })
